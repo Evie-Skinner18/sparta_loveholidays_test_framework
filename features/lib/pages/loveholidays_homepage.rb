e495c2efd5aa4d6b5b9aa766e634793245ea223d
Tech-A12 <tech-a12@Tech-A12s-MacBook-Air.local>
2018-06-19 14:21:43 +0100

    Working version complete

--- features/lib/pages/loveholidays_homepage.rb
+++ features/lib/pages/loveholidays_homepage.rb
@@ -9,6 +9,7 @@ class LoveholidaysHomepage
   HOMEPAGE_URL = "https://www.loveholidays.com/"
   HOLIDAYS_NAVBAR_LINK = '.mega-menu__top__menu-item'
   ALL_HOLIDAYS_LINK = 'View all Holidays'
+  # DESTINATION_FIELD = find(:xpath, '//*[@id="root"]/div/div/div[2]/div[2]/div/span/div/div[2]/div/div[1]/div/div/div[1]')
 
     #no need to do the @driver variable eachtime because we've already set up the driver in the spec helper
   def visit_homepage
@@ -18,7 +19,10 @@ class LoveholidaysHomepage
   def view_all_holidays
     find(:xpath,'//*[@id="menu-bar"]/nav/ul/li[1]').click
     click_link(ALL_HOLIDAYS_LINK)
-    sleep 5
+  end
+
+  def fill_destination_field
+    # fill_in(DESTINATION_FIELD, with: )
   end
 
 
